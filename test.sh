6b333be1926ff2a373c89d57917a97d20afbedfb
Wenxuan Zhang <wenxuan.zhang@kaust.edu.sa>
2024-01-03 16:11:53 +0300

    update

--- test.sh
+++ test.sh
@@ -48,10 +48,3 @@ python main.py model@_global_=causal  limit=15 model_name="meta-llama/Llama-2-70
 python main.py model_name=gpt2-xl limit=15 wandb.enabled=True
 
 
-# Alpaca
-alpaca_eval evaluate_from_model --model_configs /home/zhanw0g/github/lm-evaluation-harness/alpaca/openchat-v3.5-7b --output_path /ibex/project/c2106/wenxuan/alpaca/beaver-7b-v1.0 --max_instances 1
-alpaca_eval evaluate_from_model --model_configs /home/zhanw0g/github/lm-evaluation-harness/alpaca/beaver-7b-v1.0  --max_instances 10
-alpaca_eval evaluate_from_model --model_configs /home/zhanw0g/github/lm-evaluation-harness/alpaca/juanako-7b-UNA  --max_instances 1
-alpaca_eval evaluate_from_model --model_configs /home/zhanw0g/github/lm-evaluation-harness/alpaca/zephyr-7b-beta  --max_instances 1
-alpaca_eval evaluate_from_model --model_configs /home/zhanw0g/github/lm-evaluation-harness/alpaca/alpaca-7b-reproduced  --max_instances 1
-alpaca_eval evaluate_from_model --model_configs /home/zhanw0g/github/lm-evaluation-harness/alpaca/mistral-7b-v13  --max_instances 1
